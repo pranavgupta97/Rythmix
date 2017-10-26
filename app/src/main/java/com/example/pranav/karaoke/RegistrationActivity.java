94e7d9d86a62332bfe2bd7e42975fcc71995ec58
zeeebs <ibra.kurti@huskers.unl.edu>
2017-10-25 21:06:19 -0500

    Made checks for username and full name to be valid.

--- app/src/main/java/com/example/pranav/karaoke/RegistrationActivity.java
+++ app/src/main/java/com/example/pranav/karaoke/RegistrationActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -55,41 +56,55 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final String name = fullName.getEditText().getText().toString();
                 final String user = username.getEditText().getText().toString();
-                Query usernameQuery = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").equalTo(user);
-                usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.getChildrenCount() > 0){
-                            Toast.makeText(RegistrationActivity.this, "This username is taken. Choose a different username.",Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-                            mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser.getUid());
-                            HashMap<String, String> userMap = new HashMap<>();
-                            userMap.put("fullName", name);
-                            userMap.put("username", user);
-                            mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        //move to next activity
-                                        Intent userProfileIntent = new Intent(RegistrationActivity.this, userProfileActivity.class);
-                                        startActivity(userProfileIntent);
-                                        finish();
-                                        Toast.makeText(RegistrationActivity.this, "Database Updated", Toast.LENGTH_LONG).show();
-                                        //test
+
+                if(user.length() < 4){
+                    Toast.makeText(RegistrationActivity.this, "Username too short. Please create a username with at least 4 characters.", Toast.LENGTH_LONG).show();
+                   username.getEditText().setText("");
+                }
+                else if((Pattern.matches("[a-zA-Z]+", user) == false) ){
+                    Toast.makeText(RegistrationActivity.this, "Username must contain at least 1 letter.", Toast.LENGTH_LONG).show();
+                    username.getEditText().setText("");
+                }
+                else if(name.length() < 2 || (Pattern.matches("[a-zA-Z]+", name) == false)){
+                    Toast.makeText(RegistrationActivity.this, "Please enter your full name.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Query usernameQuery = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").equalTo(user);
+
+                    usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.getChildrenCount() > 0) {
+                                Toast.makeText(RegistrationActivity.this, "This username is taken. Choose a different username.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                                mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser.getUid());
+                                HashMap<String, String> userMap = new HashMap<>();
+                                userMap.put("fullName", name);
+                                userMap.put("username", user);
+                                mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            //move to next activity
+                                            Intent userProfileIntent = new Intent(RegistrationActivity.this, userProfileActivity.class);
+                                            startActivity(userProfileIntent);
+                                            finish();
+                                            Toast.makeText(RegistrationActivity.this, "Database Updated", Toast.LENGTH_LONG).show();
+                                            //test
+                                        }
                                     }
-                                }
-                            });
+                                });
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
 
                         }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                    });
+                }
 
             }
         });
